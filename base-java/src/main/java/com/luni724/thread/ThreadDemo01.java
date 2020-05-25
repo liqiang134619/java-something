2c09acfb9bde67893394122809043ac722ed4af7
liq <18257751477@163.com>
2020-05-25 20:59:41 +0800

    x

---
+++ base-java/src/main/java/com/luni724/thread/ThreadDemo01.java
@@ -0,0 +1,11 @@
+package com.luni724.thread;
+
+import lombok.Data;
+
+/**
+ * @author liq
+ * @date 2020/5/25
+ */
+
+public class ThreadDemo01 {
+}
---
+++ java8/src/com/example/stream/StreamTest.java
@@ -0,0 +1,33 @@
+package com.example.stream;
+
+import com.example.common.Dish;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author liq
+ * @date 2019/11/30
+ */
+public class StreamTest {
+
+
+    public static void main(String[] args) {
+
+        List<Dish> menu = Arrays.asList(
+                new Dish("pork", false, 800, Dish.Type.MEAT),
+                new Dish("beef", false, 700, Dish.Type.MEAT),
+                new Dish("chicken", false, 400, Dish.Type.MEAT),
+                new Dish("french fries", true, 530, Dish.Type.OTHER),
+                new Dish("rice", true, 350, Dish.Type.OTHER),
+                new Dish("season fruit", true, 120, Dish.Type.OTHER),
+                new Dish("pizza", true, 550, Dish.Type.OTHER),
+                new Dish("prawns", false, 300, Dish.Type.FISH),
+                new Dish("salmon", false, 450, Dish.Type.FISH) );
+
+        System.out.println("=====");
+        menu.forEach(System.out::println);
+
+
+    }
+}
